d452e18ef022b88c5f605a839f231c0a24876c54
Daniel Sifton <Daniel.Sifton@viu.ca>
2018-01-08 08:51:22 -0800

    removed key

--- config/environment.js
+++ config/environment.js
@@ -22,9 +22,10 @@ module.exports = function(environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
-    fastboot: {
-          hostWhitelist: [/^localhost:\d+$/, 'eln-sj4.is.sfu.ca:8000']
-    }
+    // remove the whiteist errors by adding your server port below
+    //fastboot: {
+    //      hostWhitelist: [/^localhost:\d+$/, 'WEBSERVER-PORT-HERE']
+   // }
   };
 
   if (environment === 'development') {
@@ -45,11 +46,9 @@ module.exports = function(environment) {
     */
 
     //VARIABLES FOR LOCAL SUPPLEJACK DATASET
-    ENV.APP.host = 'http://eln-sj3.is.sfu.ca:3000';
-    //ENV.APP.host = 'http://138.197.71.234:3000';
+    ENV.APP.host = 'YOUR-SJ-IP-HERE:3000';
     ENV.APP.namespace = '';
-    //ENV.APP.api_key = 'KQSsmdtsm78bs9P17zzK';
-    ENV.APP.api_key = 'apikey';
+    ENV.APP.api_key = 'YOUR-APIKEY-HERE';
     ENV.APP.primaryKey = 'record_id';
     ENV.APP.searchFields = 'default';
     ENV.APP.resultFields = 'all';
